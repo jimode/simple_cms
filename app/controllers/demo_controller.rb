9927d03bffaa4de019d0176c3eeb9f056aec62ed
Jimi Odeyemi <jimode68@gmail.com>
2013-08-18 03:35:23 +0100

    links and url parameters

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,9 +1,15 @@
 class DemoController < ApplicationController
   def index
-  	
+  	#render ('hello')
+  	#redirect_to(action: 'other_hello')
   end
 
   def hello
-  	
+  	@id   = params[:id]
+  	@page = params[:page].to_i
+  end
+
+  def other_hello
+  	#render(text: 'Hello Everyone')
   end
 end
